da6f4745960440387b97f72b57b778170523219c
jordana braga <danaxbraga@gmail.com>
2024-07-17 04:23:25 +0000

    adding search queries

--- products/views.py
+++ products/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
-
-# Create your views here.
+from .models import Product, Category, Artist, Publisher, Genre
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
+    categories = None
+    artists = None
+    publishers = None
+    genres = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+        
+        if 'artist' in request.GET:
+            artists = request.GET['artist'].split(',')
+            products = products.filter(artist__name__iexact=artists)
+            artists = Artist.objects.filter(name__in=artists)
+
+        if 'publisher' in request.GET:
+            publishers = request.GET['publisher'].split(',')
+            products = products.filter(publisher__name__iexact=publishers)
+            publishers = Publisher.objects.filter(name__in=publishers)
+
+        if 'genre' in request.GET:
+            genres = request.GET['genre'].split(',')
+            products = products.filter(genres__name__iexact=query)
+            genres = Genre.objects.filter(name__in=genres)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,6 +46,10 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_artists': artists,
+        'current_publishers': publishers,
+        'current_genres': genres,
     }
 
     return render(request, 'products/products.html', context)
