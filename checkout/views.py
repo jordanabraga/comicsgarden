21a34ecea7adf0df34d1d56485ff8219c7cf99ba
jordana braga <danaxbraga@gmail.com>
2024-07-18 05:49:47 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PdmzhRtDg44zOcTRJYDaQeO2CGc7PpcBMPHhK7B0EnZemX0flKLBiY32IevbTUMH9X1huuHutE4JDhumLgk1plq00mB02Pu2S',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
