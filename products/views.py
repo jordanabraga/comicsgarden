829fbaf7516cf61f41d3f7407300cd3db7ed5808
jordana braga <danaxbraga@gmail.com>
2024-07-30 22:54:12 +0000

    Add custom widget to form image

--- products/views.py
+++ products/views.py
@@ -115,7 +115,7 @@ def edit_product(request, product_id):
             messages.error(request, 'Failed to update product. Please ensure the form is valid.')
     else:
         form = ProductForm(instance=product)
-        messages.info(request, f'You are editing {product.name}')
+        messages.info(request, f'You are editing a product.')
 
     template = 'products/edit_product.html'
     context = {
