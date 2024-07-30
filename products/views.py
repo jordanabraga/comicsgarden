051b3917d2f4cbfa055ed935463fa46da3c0cb9d
jordana braga <danaxbraga@gmail.com>
2024-07-30 19:46:42 +0000

    Added add product view

--- products/views.py
+++ products/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Product, Category, Artist, Publisher, Genre
 
+from .models import Product, Category
+from .forms import ProductForm
+
+# Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -11,9 +14,6 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
-    artists = None
-    publishers = None
-    genres = None
     sort = None
     direction = None
 
@@ -21,11 +21,9 @@ def all_products(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
-            if sortkey == 'title':
-                sortkey = 'lower_title'
-                products = products.annotate(lower_title=Lower('title'))
-            elif sortkey == 'date_added':
-                sortkey = 'date_added'    
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
             if 'direction' in request.GET:
@@ -33,26 +31,11 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-        
-        if 'artist' in request.GET:
-            artists = request.GET['artist'].split(',')
-            products = products.filter(artist__name__iexact=artists)
-            artists = Artist.objects.filter(name__in=artists)
-
-        if 'publisher' in request.GET:
-            publishers = request.GET['publisher'].split(',')
-            products = products.filter(publisher__name__iexact=publishers)
-            publishers = Publisher.objects.filter(name__in=publishers)
-
-        if 'genre' in request.GET:
-            genres = request.GET['genre'].split(',')
-            products = products.filter(genres__name__iexact=query)
-            genres = Genre.objects.filter(name__in=genres)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -60,18 +43,15 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
-            queries = Q(title__icontains=query) | Q(artist__name__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
+
     current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        'current_artists': artists,
-        'current_publishers': publishers,
-        'current_genres': genres,
         'current_sorting': current_sorting,
     }
 
@@ -88,3 +68,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
