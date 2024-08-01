df61a249f81462b8ab9dd85302839ecb5eb340da
jordana braga <danaxbraga@gmail.com>
2024-08-01 23:16:17 +0000

    Add about page, views, url

--- home/views.py
+++ home/views.py
@@ -6,3 +6,6 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def about_view(request):
+    return render(request, 'home/about.html')
