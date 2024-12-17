90b3c99cad43a518bc0394b960684c3558750d69
jordana braga <danaxbraga@gmail.com>
2024-12-17 04:20:45 +0000

    Adjust checkout model

--- checkout/models.py
+++ checkout/models.py
@@ -58,18 +58,16 @@ class Order(models.Model):
         """
         return uuid.uuid4().hex.upper()
 
-    def update_total(self):
+     def update_total(self):
         """
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        lineitem_total_sum = self.lineitems.aggregate(
-            Sum('lineitem_total')
-        )['lineitem_total__sum']
-        self.order_total = lineitem_total_sum or 0
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            delivery_percentage = settings.STANDARD_DELIVERY_PERCENTAGE / 100
-            self.delivery_cost = self.order_total * delivery_percentage
+            self.delivery_cost = self.order_total \
+                * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
